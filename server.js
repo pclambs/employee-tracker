5dc99746d0ad5b53a75630343b439d28cd25ef25
deepwubs <pchucklambs@gmail.com>
2023-08-21 16:24:38 -0500

    adding function to employee_tracker

--- server.js
+++ server.js
@@ -14,7 +14,39 @@ export const employee_tracker = () => {
         message: `What would you like to do?`,
         choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Log Out']
     }]).then((answers) => {
+        if (answers.prompt === 'View All Departments') {
+            db.query(`SELECT * FROM department`, (err, result) => {
+                if (err) throw err
+                console.log("Viewing All Departments: ")
+                console.table(result)
+                employee_tracker()
+            })
+        } else if (answers.prompt === 'View All Roles') {
+            db.query(`SELECT * FROM role`, (err, result) => {
+                if (err) throw err
+                console.log("Viewing All Roles: ")
+                console.table(result)
+                employee_tracker()
+            })
+        } else if (answers.prompt === 'View All Employees') {
+            db.query(`SELECT * FROM employee`, (err, result) => {
+                if (err) throw err
+                console.log("Viewing All Employees: ")
+                console.table(result)
+                employee_tracker()
+            })
+        } else if (answers.prompt === 'Add A Department') {
 
+        } else if (answers.prompt === 'Add A Role') {
+
+        } else if (answers.prompt === 'Add An Employee') {
+
+        } else if (answers.prompt === 'Update An Employee Role') {
+
+        } else if (answers.prompt === 'Log Out') {
+            db.end()
+            console.log("Peace!")
+        }
     })
 }
 
