f1703135a73c72faaacb39279960f705fcc39379
deepwubs <pchucklambs@gmail.com>
2023-08-21 14:30:50 -0500

    added tooling and some sql

---
+++ db/connection.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2')
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'employee_tracker_db'
+})
+
+module.exports = db
