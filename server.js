3b6d8211f8caa1c7ae276b8192f7f15743b38c46
deepwubs <pchucklambs@gmail.com>
2023-08-28 18:21:26 -0500

    added more to prompts

--- server.js
+++ server.js
@@ -36,9 +36,86 @@ export const employee_tracker = () => {
                 employee_tracker()
             })
         } else if (answers.prompt === 'Add A Department') {
-
+            inquirer.prompt([{
+                type: 'input',
+                name: 'department',
+                message: 'What is the name of the dpeartment?',
+                validate: departmentInput => {
+                    if (departmentInput) {
+                        return true;
+                    } else {
+                        console.log('Please Add A Department!');
+                        return false;
+                    }
+                }
+            }]).then((answers) => {
+                db.query(`INSERT INTO department (name) VALUES (?)`, [answers.department], (err, result) => {
+                    if (err) throw err;
+                    console.log(`Added ${answers.department} to the database.`)
+                    employee_tracker();
+                });
+            })
         } else if (answers.prompt === 'Add A Role') {
+            db.query(`SELECT * FROM department`, (err, result) => {
+                if (err) throw err;
 
+                inquirer.prompt([
+                    {
+                        // add a role
+                        type: 'input',
+                        name: 'role',
+                        message: 'What is the name of the role?',
+                        validate: roleInput => {
+                            if (roleInput) {
+                                return true;
+                            } else {
+                                console.log('Please Add A Role!');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        // add salary
+                        type: 'input',
+                        name: 'salary',
+                        message: 'What is the salary of the role?',
+                        validate: salaryInput => {
+                            if (salaryInput) {
+                                return true;
+                            } else {
+                                console.log('Please Add A Salary!');
+                                return false;
+                            }
+                        }
+                    },
+                    {
+                        // dept
+                        type: 'list',
+                        name: 'department',
+                        message: 'Which department does the role belong to?',
+                        choices: () => {
+                            var array = [];
+                            for (var i = 0; i < result.length; i++) {
+                                array.push(result[i].name);
+                            }
+                            return array;
+                        }
+                    }
+                ]).then((answers) => {
+                    // compare results
+                    for (var i = 0; i < result.length; i++) {
+                        if (result[i].name === answers.department) {
+                            var department = result[i];
+                        }
+                    }
+
+                    db.query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, [answers.role, answers.salary, department.id], (err, result) => {
+                        if (err) throw err;
+                        console.log(`Added ${answers.role} to the database.`)
+                        employee_tracker();
+                    });
+                })
+            });
         } else if (answers.prompt === 'Add An Employee') {
 
         } else if (answers.prompt === 'Update An Employee Role') {
